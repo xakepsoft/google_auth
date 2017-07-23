d27109f2386505f600d9915941040f9a22bf9383
Евгений Грицацуев <zapavednik@yandex.com>
2017-07-23 09:15:39 +0300

    Update google_auth.inc.php

--- google_auth.inc.php
+++ google_auth.inc.php
@@ -9,6 +9,7 @@
 
         if(session_status() == PHP_SESSION_NONE) session_start();
 
+        //$redirect_uri  = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
         $redirect_uri  = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
         $params = [ 'redirect_uri'  => $redirect_uri , 'client_id' => $google_client_id , 'scope' => 'email profile' , 'response_type' => 'code' ];
@@ -27,28 +28,26 @@
             return  $_SESSION['google_userinfo'];
         else
         {
+            if( empty($_GET['code']) ) redirect($auth_url);
+
             $params['client_secret'] = $google_client_secret;
             $params['grant_type'] = 'authorization_code';
             $params['code'] = $_GET['code'];
             $params['response_type'] = NULL;
 
-            if( isset($_GET['code']) && !empty($_GET['code']) )
-            {
-                $opts = [
-                    'http'=>[ 'timeout'=>30, 'method'=>'POST', 'header'=>'Content-type: application/x-www-form-urlencoded', 'content'=>http_build_query( $params ) ],
-                    'ssl'=>[ 'verify_peer' => false, 'verify_peer_name' => false ] ];
+            $opts = [
+                'http'=>[ 'timeout'=>30, 'method'=>'POST', 'header'=>'Content-type: application/x-www-form-urlencoded', 'content'=>http_build_query( $params ) ],
+                'ssl'=>[ 'verify_peer' => false, 'verify_peer_name' => false ] ];
 
-                @$tokeninfo = json_decode( file_get_contents( 'https://www.googleapis.com/oauth2/v4/token' , false , stream_context_create( $opts ) ));
+            @$tokeninfo = json_decode( file_get_contents( 'https://www.googleapis.com/oauth2/v4/token' , false , stream_context_create( $opts ) ));
 
-                if ( !isset( $tokeninfo->access_token ) || strlen( $tokeninfo->access_token ) == 0 )
-                    redirect('https://www.google.com/accounts/Logout?continue='. rawurlencode( $auth_url ));
+            if ( !isset( $tokeninfo->access_token ) || strlen( $tokeninfo->access_token ) == 0 )
+                redirect('https://www.google.com/accounts/Logout?continue='. rawurlencode( $auth_url ));
 
-                $opts['http'] = [ 'timeout'=>30 ];
-                @$_SESSION['google_access_token'] = $tokeninfo->access_token;
-                @$_SESSION['google_userinfo'] = json_decode( file_get_contents( 'https://www.googleapis.com/oauth2/v3/userinfo?access_token=' . rawurlencode($tokeninfo->access_token ) , false , stream_context_create( $opts )) );
+            $opts['http'] = [ 'timeout'=>30 ];
+            @$_SESSION['google_access_token'] = $tokeninfo->access_token;
+            @$_SESSION['google_userinfo'] = json_decode( file_get_contents( 'https://www.googleapis.com/oauth2/v3/userinfo?access_token=' . rawurlencode($tokeninfo->access_token ) , false , stream_context_create( $opts )) );
 
-                redirect($params['redirect_uri']); 
-            }
-            redirect($auth_url);
+            redirect($params['redirect_uri']);
         }
     }
